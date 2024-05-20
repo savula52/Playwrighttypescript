9f3f439d15ba64cbf763ad8d6771f3f956184bea
Swapna Avula <savula@planittesting.com>
2024-05-20 10:13:52 +0100

    modified program

--- tests/githubpagetest.spec.ts
+++ tests/githubpagetest.spec.ts
@@ -7,11 +7,16 @@ test('launch github', async ({ page }) => {
   await page.locator('.HeaderMenu-link--sign-in').click()
   await page.screenshot({ path: `gitsignin.png` })
   await expect(page).toHaveURL('https://github.com/login')
-  //locator using ID
+  //locator using ID-Fill user Id
   await page.locator('id=login_field').fill('swapna')
-  //locator using xpath
-  await page.locator('//*[@id="password"]').fill('testpassword')
-  await page.locator('//*[@id="login"]/div[4]/form/div/input[13]').click()
+  //locator using CSS Selector- fillpassword 
+   await page.locator('css= #password').fill('abcdf')
+   //locator using xpath -click signin
+  await page.locator('xpath= //*[@id="login"]/div[4]/form/div/input[13]').click()
+  //await expect(page).toHaveURL('https://github.com/session')
+
+  //await page.locator('text='Create an account'').click
+
   
   await new Promise(() => {});
   
